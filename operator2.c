8557dfe2ee7c7eb34f7a8c955176351d1467db05
Vishali <jnvishali294@gmail.com>
2026-05-03 17:35:16 +0530

    Create operator2.c

---
+++ operator2.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main()
+{
+ int a,b;
+ 
+ printf("enter a number:");
+ scanf("%d",&a);
+ 
+ printf("enter a number:");
+ scanf("%d",&b);
+  
+ printf("%d\n",a<b);
+ printf("%d\n",a>b);
+ printf("%d\n",a==b);
+}
