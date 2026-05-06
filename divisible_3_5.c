208e5ed59d5e8c4b43f3d5861313b73cd7eb8a0f
Vishali <jnvishali294@gmail.com>
2026-05-06 23:55:30 +0530

    Create divisible_3_5.c

---
+++ divisible_3_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    if(num % 3 == 0 && num % 5 == 0)
+    {
+        printf("Divisible by both 3 and 5\n");
+    }
+    else
+    {
+        printf("Not divisible by both 3 and 5\n");
+    }
+
+    return 0;
+}
